c8f6d30f6ddf9ed1cb86b920943f43d1612f042d
Chris Ruenes <ruenes.chris@gmail.com>
2016-08-03 10:45:29 -0400

    Add _ensureLoggedIn to entry and protect IndexRoute

--- frontend/soundCloned.jsx
+++ frontend/soundCloned.jsx
@@ -5,15 +5,21 @@ const LoginForm = require('./components/login_form');
 const SignupForm = require('./components/signup_form');
 const TracksIndex = require('./components/tracks_index');
 const SessionActions = require('./actions/session_actions');
+const SessionStore = require('./stores/session_store');
 import { Router, Route, IndexRoute, hashHistory} from "react-router";
 
+const _ensureLoggedIn = function(nextState, replace){
+  if (!SessionStore.isUserLoggedIn()){
+    replace("/login");
+  }
+};
 
 const appRouter = (
   <Router history={hashHistory}>
     <Route path="/" component={ App } >
-      <IndexRoute component = { TracksIndex } />
+      <IndexRoute component = { TracksIndex } onEnter={ _ensureLoggedIn}/>
       <Route path="/login" component={ LoginForm } />
-      <Route path="/signup" component={SignupForm } />
+      <Route path="/signup" component={ SignupForm } />
     </ Route>
   </Router>
 );
