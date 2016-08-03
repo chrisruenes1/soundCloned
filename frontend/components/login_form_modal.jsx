3157146eb0f9db83eb9e6e78bb781744136775c6
= <=>
2016-08-03 12:18:37 -0400

    Fail to integrate modal

---
+++ frontend/components/login_form_modal.jsx
@@ -0,0 +1,34 @@
+const React = require('react');
+import Modal, {closeStyel} from 'simple-react-modal';
+
+const LoginForm = require('./login_form');
+
+const LoginFormModal = React.createClass({
+  getInitialState(){
+    return {show:false};
+  },
+  show(){
+    this.setState({show:true});
+  },
+
+  close(){
+    this.setState({show:false});
+  },
+  render(){
+    return(
+      <Modal
+        className='modal'
+        conatinerStyle='modal-container'
+        closeOnOuterClick={true}
+        show={this.state.show}
+        onClose={this.close}
+      >
+      <LoginForm />
+
+      </Modal>
+    );
+  }
+
+});
+
+module.exports = LoginFormModal;
