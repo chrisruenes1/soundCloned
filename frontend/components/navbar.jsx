fc40348920faceb0ce5983254921dc4e36d7ba6b
= <=>
2016-08-03 11:53:56 -0400

    Add and style navbar. Install modal dependencies

---
+++ frontend/components/navbar.jsx
@@ -0,0 +1,16 @@
+const React = require('react');
+import { Link } from 'react-router';
+const Navbar = React.createClass({
+  render(){
+    return(
+      <nav className="navbar">
+        <ul className="group">
+          <li className="navbar-element navbar-button login"><Link to="/login">Sign in</Link></li>
+          <li className="navbar-element navbar-minor">or</li>
+          <li className="navbar-element navbar-button signup"><Link to="/signup">Create account</Link></li>
+        </ul>
+      </nav>
+    );
+  }
+});
+module.exports = Navbar;
