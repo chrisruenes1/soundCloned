16bf918bab111ad0fed3887774201d5694d9f1c8
Chris Ruenes <ruenes.chris@gmail.com>
2016-08-03 12:18:37 -0400

    Fail to integrate modal

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,11 +1,12 @@
 const React = require('react');
 import { Link } from 'react-router';
+const LoginFormModal = require('./login_form_modal');
 const Navbar = React.createClass({
   render(){
     return(
       <nav className="navbar">
         <ul className="group">
-          <li className="navbar-element navbar-button login"><Link to="/login">Sign in</Link></li>
+          <li className="navbar-element navbar-button login"><button onClick={LoginFormModal.show}>Sign in</button><LoginFormModal/></li>
           <li className="navbar-element navbar-minor">or</li>
           <li className="navbar-element navbar-button signup"><Link to="/signup">Create account</Link></li>
         </ul>
