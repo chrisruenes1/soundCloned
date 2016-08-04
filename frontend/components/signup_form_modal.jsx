fd6adb44cf0bc22c6ec7e10abfa2dbfd0e5dde4e
Chris Ruenes <ruenes.chris@gmail.com>
2016-08-04 10:32:10 -0400

    Update modal styling

--- frontend/components/signup_form_modal.jsx
+++ frontend/components/signup_form_modal.jsx
@@ -24,6 +24,7 @@ const SignupFormModal = React.createClass({
           isOpen={this.state.modalIsOpen}
           onAfterOpen={this.afterOpenModal}
           onRequestClose={this.closeModal}
+          style={getStyles()}
         >
         <SignupForm close={this.closeModal}/>
         </Modal>
@@ -32,5 +33,34 @@ const SignupFormModal = React.createClass({
   }
 
 });
+
+let getStyles = function(){
+  return {
+    overlay : {
+      position          : 'fixed',
+      top               : 0,
+      left              : 0,
+      right             : 0,
+      bottom            : 0,
+      backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+    },
+    content : {
+      width                      : '500px',
+      height                     : '500px',
+      position                   : 'absolute',
+      left                       : '50%',
+      top                        : '40%',
+      transform                  : "translate(-50%, -50%)",
+      border                     : '1px solid rgba(240, 238, 238, 1)',
+      background                 : 'rgba(51, 51, 51, 0.85)',
+      overflow                   : 'auto',
+      WebkitOverflowScrolling    : 'touch',
+      borderRadius               : '4px',
+      outline                    : 'none',
+      padding                    : '20px'
+
+    }
+  };
+};
 
 module.exports = SignupFormModal;
