86e7cc75559ea399b948108bb3bc398c2a2ca154
Chris Ruenes <ruenes.chris@gmail.com>
2016-08-04 10:20:38 -0400

    Style and position modal. Add linter config

--- frontend/components/login_form_modal.jsx
+++ frontend/components/login_form_modal.jsx
@@ -1,7 +1,6 @@
 const React = require('react');
 const Modal = require('react-modal');
 const LoginForm = require('./login_form');
-
 const LoginFormModal = React.createClass({
   getInitialState(){
     return {modalIsOpen:false};
@@ -24,6 +23,7 @@ const LoginFormModal = React.createClass({
           isOpen={this.state.modalIsOpen}
           onAfterOpen={this.afterOpenModal}
           onRequestClose={this.closeModal}
+          style={getStyles()}
         >
         <LoginForm close={this.closeModal}/>
         </Modal>
@@ -32,5 +32,34 @@ const LoginFormModal = React.createClass({
   }
 
 });
+
+let getStyles = function(){
+  return {
+    overlay : {
+      position          : 'fixed',
+      top               : 0,
+      left              : 0,
+      right             : 0,
+      bottom            : 0,
+      backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+    },
+    content : {
+      width                      : '500px',
+      height                     : '500px',
+      position                   : 'absolute',
+      left                       : '50%',
+      top                        : '40%',
+      transform                  : "translate(-50%, -50%)",
+      border                     : '1px solid rgba(240, 238, 238, 1)',
+      background                 : '#fff',
+      overflow                   : 'auto',
+      WebkitOverflowScrolling    : 'touch',
+      borderRadius               : '4px',
+      outline                    : 'none',
+      padding                    : '20px'
+
+    }
+  };
+};
 
 module.exports = LoginFormModal;
