b984819f4fec553df4766d5d4e69f6b142983fc5
App Academy Student <appacademy@Apps-Mac-mini-167.local>
2016-08-05 09:18:36 -0400

    Style profile header and add edit profile button for current user

--- frontend/components/profile_header.jsx
+++ frontend/components/profile_header.jsx
@@ -1,15 +1,26 @@
 const React = require('react');
+const FormModal = require('./form_modal');
+const ProfileForm = require('./profile_form');
+const SessionStore = require('../stores/session_store');
+
 
 const ProfileHeader = React.createClass({
+
   render(){
+    let editInfo = this.props.user.id === SessionStore.currentUser().id ?
+      <FormModal className="navbar-button reactive-navbar-button edit-button" buttonText="Edit Profile"><ProfileForm /></FormModal>
+      :
+      <div></div>;
     return(
       <header className="group profile-header-image">
         <img className="profile-picture" src="http://funny-pics.co/wp-content/uploads/sinister-otter.jpg" />
         <div className="profile-user-info">
-          <span className="profile-element">{this.props.user.group_name}</span>
-          <span className="profile-element">{this.props.user.fname} {this.props.user.lname}</span>
-          <span className="profile-element">{this.props.user.city}, {this.props.user.state}</span>
+          <span className="profile-element profile-large">{this.props.user.group_name}</span>
+          <span className="profile-element profile-small">{this.props.user.fname} {this.props.user.lname}</span>
+          <span className="profile-element profile-small">{this.props.user.city}, {this.props.user.state}</span>
         </div>
+        {editInfo}
+
       </header>
     );
   }
