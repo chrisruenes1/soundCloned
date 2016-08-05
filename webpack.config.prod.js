becbeaffaf22cbb344cf8cef991c35e6f747cff9
Chris Ruenes <ruenes.chris@gmail.com>
2016-08-05 15:06:18 -0400

    Fix minified warning bug in production

---
+++ webpack.config.prod.js
@@ -0,0 +1,30 @@
+var path = require("path");
+var webpack = require("webpack");
+
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/soundCloned.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  devtool: 'source-map'
+};
