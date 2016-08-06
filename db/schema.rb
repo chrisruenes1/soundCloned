4e5fb094a9ef7239030eff8010ffaa113fb96025
Chris Ruenes <ruenes.chris@gmail.com>
2016-08-06 15:13:16 -0400

    Add image column to track model. Add d paperclip and configure to work with aws to store images and audio

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806182505) do
+ActiveRecord::Schema.define(version: 20160806183556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tracks", force: :cascade do |t|
-    t.string  "title",       null: false
-    t.integer "composer_id", null: false
-    t.string  "album"
+    t.string   "title",              null: false
+    t.integer  "composer_id",        null: false
+    t.string   "album"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "tracks", ["album"], name: "index_tracks_on_album", using: :btree
