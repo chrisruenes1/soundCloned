4676d2f30be3d5ddfe945c9b6d8f457f43eca53e
chrisruenes1 <ruenes.chris@gmail.com>
2016-08-06 15:13:16 -0400

    Add image column to track model. Add d paperclip and configure to work with aws to store images and audio

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,6 @@
 class Track < ActiveRecord::Base
+  has_attached_file :image, default_url: "danny-elfman.jpg"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :title, :composer_id, presence:true
 
   belongs_to(
@@ -7,4 +9,5 @@ class Track < ActiveRecord::Base
     primary_key: :id,
     foreign_key: :composer_id
   )
+
 end
