2e42cd1acbcabfaca15e3137a16649cc86dd19cb
Chris Ruenes <ruenes.chris@gmail.com>
2016-08-06 15:31:09 -0400

    Add audio_file column to tracks table and integrate paperclip.

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,8 @@
 class Track < ActiveRecord::Base
   has_attached_file :image, default_url: "danny-elfman.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  has_attached_file :audio_file, default_url: "NoMoreHeroes-NineToLife.mp3"
+  validates_attachment_content_type :audio_file, content_type: "audio/mpeg"
   validates :title, :composer_id, presence:true
 
   belongs_to(
