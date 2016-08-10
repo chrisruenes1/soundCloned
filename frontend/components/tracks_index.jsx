20a224d2cb4494b2e51998ba5058138e8a789b5e
App Academy Student <=>
2016-08-10 16:14:57 -0400

    In-progress solution for extracting duration info from tracks

--- frontend/components/tracks_index.jsx
+++ frontend/components/tracks_index.jsx
@@ -4,12 +4,19 @@ const TrackIndexItem = require('./track_index_item');
 const TrackActions = require('../actions/track_actions');
 const CommentActions = require('../actions/comment_actions');
 
+let _tracksWithDuration = [];
+
 const TracksIndex = React.createClass({
   getInitialState(){
     this.listeners = [];
     return { tracks: TrackStore.all()};
   },
   render(){
+    //extract duration information to pass to each element
+    let tempAudio = new Audio();
+    let idx = 0;
+
+    let processedTracks = this.getDurationForTracks(this.state.tracks, idx, tempAudio);
     return(
       <ul>
         {
@@ -25,6 +32,34 @@ const TracksIndex = React.createClass({
       </ul>
     );
   },
+  getDurationForTracks(tracks, currentIdx, audioElement){
+    debugger
+    if (tracks.length > 0){
+      let track = tracks[currentIdx];
+      if (track.duration){
+        _tracksWithDuration.push(track);
+        if (tracks[currentIdx+1]){
+          this.getDurationForTracks(tracks, currentIdx+1, audioElement);
+        }
+        else {
+          return _tracksWithDuration;
+        }
+      }
+      else {
+        audioElement.setAttribute('src', track.audio_file_url);
+        audioElement.addEventListener('loadedmetadata', () => {
+          track.duration = audioElement.duration;
+          _tracksWithDuration.push(track);
+          if (tracks[currentIdx+1]){
+            this.getDurationForTracks(tracks, currentIdx+1, audioElement);
+          }
+          else {
+            return _tracksWithDuration;
+          }
+        });
+      }
+    }
+  },
   componentDidMount(){
     this.listeners.push(TrackStore.addListener(this._onChange));
     TrackActions.fetchAllTracks();
@@ -39,9 +74,5 @@ const TracksIndex = React.createClass({
     this.setState({ tracks: TrackStore.all() });
   }
 });
-
-const trackIndexItemStyles = {
-
-};
 
 module.exports = TracksIndex;
