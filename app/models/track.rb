c8527a327870264e410ef705b50098826d748688
App Academy Student <ruenes.chris@gmail.com>
2016-08-12 13:40:14 -0400

    Add error checking and messages for bad audio upload; add x to top of modals

--- app/models/track.rb
+++ app/models/track.rb
@@ -20,11 +20,13 @@
 #
 
 class Track < ActiveRecord::Base
-  has_attached_file :image, default_url: "missing.jpg"
+  has_attached_file :image, default_url: "missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 
   has_attached_file :audio_file, default_url: "NoMoreHeroes-NineToLife.mp3"
   validates_attachment_content_type :audio_file, content_type: /\Aaudio\/.*\Z/
+
 
   validates :title, :composer_id, :duration, :audio_file, presence: true
 
